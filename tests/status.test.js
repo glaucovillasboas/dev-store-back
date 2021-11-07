e1af105ac3c5e03f09b8bb0df3f0da8a32cf8b24
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-06 23:27:12 -0300

    testing husky

--- tests/status.test.js
+++ tests/status.test.js
@@ -4,7 +4,7 @@ import app from '../src/app.js';
 
 describe('GET /status', () => {
     test('returns 200 if server is working', async () => {
-        const result = await supertest(app).get('/status')
+        const result = await supertest(app).get('/statusss')
         expect(result.status).toEqual(200);
     });
 });
