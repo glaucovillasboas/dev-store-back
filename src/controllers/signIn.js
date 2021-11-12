2b76087bae8fdf0f17681aaff9cdd1592c6af343
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-12 19:50:52 -0300

    created /user tests and factories

--- src/controllers/signIn.js
+++ src/controllers/signIn.js
@@ -26,6 +26,27 @@ const signIn = async (req, res) => {
       return res.sendStatus(404);
     }
 
+    const addressesQuery = await connection.query(
+      `
+        SELECT
+          addresses.address, addresses.cep, addresses.complement,
+          states.name AS state
+        FROM addresses
+          JOIN states
+            ON states.id = addresses.state_id
+        WHERE user_id = $1`, [user.id]
+    );
+
+    const address = addressesQuery.rows[0];
+
+    const phonesQuery = await connection.query(
+      `
+        SELECT *
+        FROM phones WHERE user_id = $1`, [user.id]
+    );
+
+    const { phone } = phonesQuery.rows[0];
+
     if (!bcrypt.compareSync(password, user.password)) {
       return res.sendStatus(401);
     }
@@ -35,9 +56,7 @@ const signIn = async (req, res) => {
     const token = jwt.sign(data, jwtSecret);
 
     const sessionQuery = await connection.query(
-      `
-            SELECT * FROM sessions WHERE user_id = $1;
-        `,
+      'SELECT * FROM sessions WHERE user_id = $1;',
       [user.id]
     );
 
@@ -45,27 +64,24 @@ const signIn = async (req, res) => {
 
     if (!session) {
       await connection.query(
-        `
-            INSERT INTO sessions (user_id, token) VALUES ($1, $2);
-        `,
+        'INSERT INTO sessions (user_id, token) VALUES ($1, $2);',
         [user.id, token]
       );
     } else {
       await connection.query(
-        `
-            UPDATE sessions SET token = $1 WHERE user_id = $2;
-        `,
+        'UPDATE sessions SET token = $1 WHERE user_id = $2;',
         [token, user.id]
       );
     }
 
     return res.status(200).send({
       token,
+      address,
+      phone,
       name: user.name,
       photo: user.photo,
     });
   } catch (err) {
-    console.log(err);
     return res.sendStatus(500);
   }
 };
---
+++ tests/getUser.test.js
@@ -0,0 +1,38 @@
+import '../src/setup.js';
+import supertest from 'supertest';
+import app from '../src/app.js';
+import connection from '../src/database.js';
+import {
+    validSessionFactory,
+} from '../src/factories/session.factory.js';
+
+afterAll(async () => {
+    connection.end();
+});
+
+describe('GET /user', () => {
+    beforeAll(async () => {
+        await connection.query('DELETE FROM sessions;');
+        await connection.query('DELETE FROM addresses;');
+        await connection.query('DELETE FROM phones;');
+        await connection.query('DELETE FROM users;');
+    });
+
+    test('returns 200 with valid session', async () => {
+        const validSession = await validSessionFactory();
+        const result = await supertest(app).get('/user').set('Authorization', `Bearer ${validSession.token}`);
+        console.log(result.body);
+        expect(result.status).toEqual(200);
+        expect(result.body).toHaveProperty('name');
+        expect(result.body).toHaveProperty('phone');
+        expect(result.body).toHaveProperty('photo');
+        expect(result.body).toHaveProperty('address');
+    });
+
+    afterAll(async () => {
+        await connection.query('DELETE FROM sessions;');
+        await connection.query('DELETE FROM addresses;');
+        await connection.query('DELETE FROM phones;');
+        await connection.query('DELETE FROM users;');
+    });
+});
