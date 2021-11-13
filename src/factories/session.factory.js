358fa61a65d6302f963c884823a937dcd390c5c2
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-13 16:43:12 -0300

    removed try catch from session factory

--- src/factories/session.factory.js
+++ src/factories/session.factory.js
@@ -3,47 +3,43 @@ import { existingUserFactory } from './registration.factory.js';
 import connection from '../database.js';
 
 const validSessionFactory = async () => {
-  try {
-    const user = await existingUserFactory();
+  const user = await existingUserFactory();
 
-    const userQuery = await connection.query(
-      `   SELECT users.*
+  const userQuery = await connection.query(
+    `   SELECT users.*
         FROM users
         WHERE users.email = $1;`,
-      [user.email],
+    [user.email],
+  );
+
+  const userId = userQuery.rows[0].id;
+
+  const data = { id: userId, email: user.email, name: user.name };
+  const jwtSecret = process.env.JWT_SECRET;
+  const token = jwt.sign(data, jwtSecret);
+
+  const sessionQuery = await connection.query(
+    'SELECT * FROM sessions WHERE user_id = $1;',
+    [userId],
+  );
+
+  const session = sessionQuery.rows[0];
+
+  if (!session) {
+    await connection.query(
+      'INSERT INTO sessions (user_id, token) VALUES ($1, $2);',
+      [userId, token],
     );
-
-    const userId = userQuery.rows[0].id;
-
-    const data = { id: userId, email: user.email, name: user.name };
-    const jwtSecret = process.env.JWT_SECRET;
-    const token = jwt.sign(data, jwtSecret);
-
-    const sessionQuery = await connection.query(
-      'SELECT * FROM sessions WHERE user_id = $1;',
-      [userId],
+  } else {
+    await connection.query(
+      'UPDATE sessions SET token = $1 WHERE user_id = $2;',
+      [token, userId],
     );
-
-    const session = sessionQuery.rows[0];
-
-    if (!session) {
-      await connection.query(
-        'INSERT INTO sessions (user_id, token) VALUES ($1, $2);',
-        [userId, token],
-      );
-    } else {
-      await connection.query(
-        'UPDATE sessions SET token = $1 WHERE user_id = $2;',
-        [token, userId],
-      );
-    }
-
-    return {
-      token,
-    };
-  } catch (error) {
-    console.log(error);
   }
+
+  return {
+    token,
+  };
 };
 
 const invalidSessionFactory = () => ({
