2002ec99adbb428162a98bbf79fe140c82e1624f
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-13 13:05:07 -0300

    selecting current user cart, if it doesn't exists, creates a new one

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,5 +1,6 @@
 /* eslint-disable comma-dangle */
 import jwt from 'jsonwebtoken';
+import connection from '../database.js';
 
 const addCart = async (req, res) => {
   const { authorization } = req.headers;
@@ -8,7 +9,27 @@ const addCart = async (req, res) => {
   const jwtSecret = process.env.JWT_SECRET;
   const user = jwt.verify(token, jwtSecret);
 
-  res.send(user);
+  let cartQuery = await connection.query(
+    'SELECT * FROM carts WHERE user_id = $1 AND "finishedAt" IS NULL', [user.id]
+  );
+
+  let cart = cartQuery.rows[0];
+  console.log(cart);
+
+  if (!cart) {
+    await connection.query(
+      'INSERT INTO carts (user_id) VALUES ($1)', [user.id]
+    );
+
+    cartQuery = await connection.query(
+      'SELECT * FROM carts WHERE user_id = $1 AND "finishedAt" IS NULL', [user.id]
+    );
+
+    cart = cartQuery.rows[0];
+  }
+
+  console.log(cart);
+  res.send(cart);
 };
 
 export default addCart;
