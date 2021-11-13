a45fb9802256d6fd9b159447caa4b712b63d7aa3
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-11-12 22:23:41 -0300

    successfull tests expects phone and address to be received

--- tests/signIn.test.js
+++ tests/signIn.test.js
@@ -30,6 +30,8 @@ describe('POST /sign-in', () => {
     expect(result.body).toHaveProperty('token');
     expect(result.body).toHaveProperty('name');
     expect(result.body).toHaveProperty('photo');
+    expect(result.body).toHaveProperty('phone');
+    expect(result.body).toHaveProperty('address');
   });
 
   test('returns 200 with valid user and password and user is already logged in', async () => {
@@ -40,6 +42,8 @@ describe('POST /sign-in', () => {
     expect(result.body).toHaveProperty('token');
     expect(result.body).toHaveProperty('name');
     expect(result.body).toHaveProperty('photo');
+    expect(result.body).toHaveProperty('address');
+    expect(result.body).toHaveProperty('phone');
   });
 
   test('returns 400 with invalid user', async () => {
